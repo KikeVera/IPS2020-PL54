d140793339cd1ae6c6a1102d773d257187435b91
Moises <uo270824@uniovi.es>
2020-11-07 20:54:11 +0100

    Mas control de acciones de usuario y preparacion para visualizacion del importe en el pago del pedido.

--- src/main/java/logica/pagoPedido/PagoPedidoController.java
+++ src/main/java/logica/pagoPedido/PagoPedidoController.java
@@ -11,30 +11,31 @@ import ui.pagoPedido.PagoPedidoView;
 import ui.producto.ProductosView;
 
 /**
- * Es el controlador de la ventana de pago de pedido , accesible 
- * desde la tienda online 
+ * Es el controlador de la ventana de pago de pedido , accesible desde la tienda
+ * online
+ * 
  * @author Moises
  *
  */
-public class PagoPedidoController implements Controller{
+public class PagoPedidoController implements Controller {
 
-	private PagoPedidoView view; //Vista de la ventana de pago pedido 
-	private ProductosView tienda; //Referencia a la vista de la tienda online 
- 
+	private PagoPedidoView view; // Vista de la ventana de pago pedido
+	private ProductosView tienda; // Referencia a la vista de la tienda online
+
 	/**
-	 * Constructor 
-	 * @param v Vista 
-	 * @param tienda Referencia a la vista de la tienda online 
+	 * Constructor
+	 * 
+	 * @param v      Vista
+	 * @param tienda Referencia a la vista de la tienda online
 	 */
-	public PagoPedidoController(PagoPedidoView v,ProductosView tienda) {
-		this.view = v; 
-		this.tienda = tienda; 
-		this.initController();	
+	public PagoPedidoController(PagoPedidoView v, ProductosView tienda) {
+		this.view = v;
+		this.tienda = tienda;
+		this.initController();
 	}
 
-
 	/**
-	 * Inicia los controladores 
+	 * Inicia los controladores
 	 */
 	@Override
 	public void initController() {
@@ -59,7 +60,7 @@ public class PagoPedidoController implements Controller{
 		this.view.getBtnAtrasTarjeta().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				back(); 		
+				back();
 			}
 		});
 		this.view.getBtnSiguienteTransaccion().addActionListener(new ActionListener() {
@@ -76,109 +77,114 @@ public class PagoPedidoController implements Controller{
 				back();
 			}
 		});
-		
+
 	}
 
-
 	/**
-	 * Inicia la vista 
+	 * Inicia la vista
 	 */
 	@Override
 	public void initView() {
-		this.view.getFrame().setVisible(true);		
+		this.view.getFrame().setVisible(true);
 	}
-	
+
 	/**
 	 * Avanza al siguiente panel seleccionado
 	 */
 	private void next() {
-		
-		CardLayout c = (CardLayout) this.view.getFrame().getContentPane().getLayout(); 
-		
-		if(this.view.getRdbTarjeta().isSelected()) {
+
+		CardLayout c = (CardLayout) this.view.getFrame().getContentPane().getLayout();
+
+		if (this.view.getRdbTarjeta().isSelected()) {
 			c.show(this.view.getFrame().getContentPane(), "Tarjeta");
-		}
-		else if (this.view.getRdbTransferencia().isSelected()) {
+		} else if (this.view.getRdbTransferencia().isSelected()) {
 			c.show(this.view.getFrame().getContentPane(), "Transaccion");
-		}
-		else if (this.view.getRdbContrareembolso().isSelected()) {
+		} else if (this.view.getRdbContrareembolso().isSelected()) {
 			this.tienda.getBtnFinalizarPedido().setEnabled(true);
 			tienda.getBtnPagarPedido().setEnabled(false);
 			cerrar();
+		} else {
+			JOptionPane.showMessageDialog(this.view.getFrame(), "Debe seleccionar un metodo de pago",
+					"Tienda online: Advertencia", JOptionPane.WARNING_MESSAGE);
+			return;
 		}
+
 	}
-	
+
 	/**
-	 * Retrocede al anterior panel 
+	 * Retrocede al anterior panel
 	 */
 	private void back() {
-		CardLayout c = (CardLayout) this.view.getFrame().getContentPane().getLayout(); 
+		CardLayout c = (CardLayout) this.view.getFrame().getContentPane().getLayout();
 		c.show(this.view.getFrame().getContentPane(), "Inicio");
 	}
-	
+
 	/**
-	 * Cierra la ventana del pago de pedido actual 
+	 * Cierra la ventana del pago de pedido actual
 	 */
 	private void cerrar() {
 		this.view.getFrame().dispose();
 	}
-	
+
 	/**
-	 * Comprueba que los datos de la tarjeta introducidos por el usuario son correctos y en caso de que 
-	 * las validaciones salieran correctamente, habilita la opcion de finalizar pedido en la tienda online  
+	 * Comprueba que los datos de la tarjeta introducidos por el usuario son
+	 * correctos y en caso de que las validaciones salieran correctamente, habilita
+	 * la opcion de finalizar pedido en la tienda online
 	 */
 	private void checkTarjeta() {
-		
-		//El numero de tarjeta no debe de ser vacio 
-		if(view.getTextNumeroTarjeta().getText().isBlank() || view.getTextNumeroTarjeta().getText().isEmpty()) {
+
+		// El numero de tarjeta no debe de ser vacio
+		if (view.getTextNumeroTarjeta().getText().isBlank() || view.getTextNumeroTarjeta().getText().isEmpty()) {
 			JOptionPane.showMessageDialog(this.view.getFrame(), "No debe dejar el numero de la tarjeta vacío",
 					"Tienda online: Advertencia", JOptionPane.WARNING_MESSAGE);
-			return; 
+			return;
 		}
-		
-		//El CVV no debe de ser vacio 
-		if(view.getTextCVV().getText().isBlank() || view.getTextCVV().getText().isEmpty()) {
+
+		// El CVV no debe de ser vacio
+		if (view.getTextCVV().getText().isBlank() || view.getTextCVV().getText().isEmpty()) {
 			JOptionPane.showMessageDialog(this.view.getFrame(), "No debe dejar el CVV de la tarjeta vacío",
 					"Tienda online: Advertencia", JOptionPane.WARNING_MESSAGE);
-			return; 
+			return;
 		}
-		
-		//El numero de tarjeta debe de ser un campo numerico
-		for(int i = 0;i<view.getTextNumeroTarjeta().getText().length();i++) {
-			if(!Character.isDigit(view.getTextNumeroTarjeta().getText().charAt(i))) {
+
+		// El numero de tarjeta debe de ser un campo numerico
+		for (int i = 0; i < view.getTextNumeroTarjeta().getText().length(); i++) {
+			if (!Character.isDigit(view.getTextNumeroTarjeta().getText().charAt(i))) {
 				JOptionPane.showMessageDialog(this.view.getFrame(), "El numero de la tarjeta es un campo numerico",
 						"Tienda online: Advertencia", JOptionPane.WARNING_MESSAGE);
-				return;  
+				return;
 			}
 		}
-		
-		//El CVV debe de ser un campo numerico 
-		for(int i = 0;i<view.getTextCVV().getText().length();i++) {
-			if(!Character.isDigit(view.getTextCVV().getText().charAt(i))) {
+
+		// El CVV debe de ser un campo numerico
+		for (int i = 0; i < view.getTextCVV().getText().length(); i++) {
+			if (!Character.isDigit(view.getTextCVV().getText().charAt(i))) {
 				JOptionPane.showMessageDialog(this.view.getFrame(), "El CVV de la tarjeta es un campo numerico",
 						"Tienda online: Advertencia", JOptionPane.WARNING_MESSAGE);
-				return; 
+				return;
 			}
 		}
-		
-		//El numero de tarjeta debe estar compuesto de 16 digitos 
-		if(view.getTextNumeroTarjeta().getText().length() != 16) {
-			JOptionPane.showMessageDialog(this.view.getFrame(), "El número de la tarjeta debe estar compuesto por 16 dígitos",
-					"Tienda online: Advertencia", JOptionPane.WARNING_MESSAGE);
-			return; 
+
+		// El numero de tarjeta debe estar compuesto de 16 digitos
+		if (view.getTextNumeroTarjeta().getText().length() != 16) {
+			JOptionPane.showMessageDialog(this.view.getFrame(),
+					"El número de la tarjeta debe estar compuesto por 16 dígitos", "Tienda online: Advertencia",
+					JOptionPane.WARNING_MESSAGE);
+			return;
 		}
-		
-		//El CVV debe estar compuesto de 3 digitos
-		if(view.getTextCVV().getText().length() != 3) {
-			JOptionPane.showMessageDialog(this.view.getFrame(), "El CVV de la tarjeta debe estar compuesto por 3 dígitos",
-					"Tienda online: Advertencia", JOptionPane.WARNING_MESSAGE);
-			return; 
+
+		// El CVV debe estar compuesto de 3 digitos
+		if (view.getTextCVV().getText().length() != 3) {
+			JOptionPane.showMessageDialog(this.view.getFrame(),
+					"El CVV de la tarjeta debe estar compuesto por 3 dígitos", "Tienda online: Advertencia",
+					JOptionPane.WARNING_MESSAGE);
+			return;
 		}
-		
-		//Si todo se valido correctamente
+
+		// Si todo se valido correctamente
 		tienda.getBtnFinalizarPedido().setEnabled(true);
 		tienda.getBtnPagarPedido().setEnabled(false);
 		cerrar();
 	}
-	
+
 }
--- src/main/java/ui/pagoPedido/PagoPedidoView.java
+++ src/main/java/ui/pagoPedido/PagoPedidoView.java
@@ -2,7 +2,6 @@ package ui.pagoPedido;
 
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 
@@ -17,21 +16,22 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
 import net.miginfocom.swing.MigLayout;
 
 /**
- * Representa la ventana de pago del pedido, accesible desde la tienda online 
+ * Representa la ventana de pago del pedido, accesible desde la tienda online
  * Tener en cuenta que esta es un JDialog
+ * 
  * @author Moises
  *
  */
-public class PagoPedidoView extends JDialog{
-	
+public class PagoPedidoView extends JDialog {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JDialog frmPago;
 	private JPanel pnInicio;
 	private JPanel pnMetodosPago;
@@ -59,60 +59,100 @@ public class PagoPedidoView extends JDialog{
 	private JScrollPane scpTransaccion;
 	private JLabel lblInformacinParaRealizar;
 	private JTextArea textAreaTransaccion;
+	private JPanel pnImporteInicio;
+	private JLabel lblImporte;
+	private JTextField textImporte;
+	private JPanel pnImporteTarjeta;
+	private JLabel lblImporteTarjeta;
+	private JTextField textField_1;
+	private JPanel pnImporteTransaccion;
+	private JLabel lblImporteTransaccion;
+	private JTextField textImporteTransaccion;
 
-	
 	/**
-	 * Constructor 
+	 * Constructor
 	 */
 	public PagoPedidoView() {
-		initialize();		
+		initialize();
 	}
-	
-	
+
 	/**
 	 * Initialize the contents of the frame.
+	 * 
 	 * @wbp.parser.entryPoint
 	 */
 	private void initialize() {
 
-		//Inicializa la ventana principal 
+		// Inicializa la ventana principal
+
+		scpTransaccion = new JScrollPane();
+
+		textAreaTransaccion = new JTextArea();
+		textAreaTransaccion.setEditable(false);
+		textAreaTransaccion.setText(
+				"Debe realizar un ingreso con el correspondiente importe del pedido al siguiente n\u00FAmero de cuenta: \r\n\r\nXXXX-XXXX-XX-XXXXXXXXXX ");
+		scpTransaccion.setViewportView(textAreaTransaccion);
+
+		lblInformacinParaRealizar = new JLabel("Informaci\u00F3n para realizar la transacci\u00F3n:");
+		lblInformacinParaRealizar.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblInformacinParaRealizar.setFont(new Font("Tahoma", Font.BOLD, 16));
+
 		pnInicio = new JPanel();
-		pnInicio.setLayout(new MigLayout("", "[591px]", "[115px][99px]"));
-		
+		pnInicio.setLayout(new MigLayout("", "[591px]", "[204px][99px]"));
+
 		pnMetodosPago = new JPanel();
-		pnMetodosPago.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Seleccion un método de pago", TitledBorder.LEADING, TitledBorder.TOP, new java.awt.Font("Tahoma", java.awt.Font.BOLD,16), new Color(0, 0, 0)));
+		pnMetodosPago.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"),
+				"Seleccion un método de pago", TitledBorder.LEADING, TitledBorder.TOP,
+				new java.awt.Font("Tahoma", java.awt.Font.BOLD, 16), new Color(0, 0, 0)));
 		pnInicio.add(pnMetodosPago, "cell 0 0,grow");
 		pnMetodosPago.setLayout(new GridLayout(3, 1, 0, 0));
-		
+
 		rdbTarjeta = new JRadioButton("Tarjeta");
 		buttonGroup.add(rdbTarjeta);
 		rdbTarjeta.setFont(new Font("Tahoma", Font.BOLD, 16));
 		pnMetodosPago.add(rdbTarjeta);
-		
+
 		rdbContrareembolso = new JRadioButton("Contra reembolso");
 		buttonGroup.add(rdbContrareembolso);
 		rdbContrareembolso.setFont(new Font("Tahoma", Font.BOLD, 16));
 		pnMetodosPago.add(rdbContrareembolso);
-		
+
 		rdbTransferencia = new JRadioButton("Transferencia");
 		buttonGroup.add(rdbTransferencia);
 		rdbTransferencia.setFont(new Font("Tahoma", Font.BOLD, 16));
 		pnMetodosPago.add(rdbTransferencia);
-		
+
 		pnBotonesInicio = new JPanel();
+		pnBotonesInicio.setBorder(new LineBorder(new Color(0, 0, 0), 3));
 		pnInicio.add(pnBotonesInicio, "cell 0 1,grow");
-		pnBotonesInicio.setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 35));
-		
+		pnBotonesInicio.setLayout(new MigLayout("", "[350px,center][75px][109px]", "[54px,grow]"));
+
+		pnImporteInicio = new JPanel();
+		pnImporteInicio.setBorder(new LineBorder(new Color(0, 0, 0)));
+		pnBotonesInicio.add(pnImporteInicio, "cell 0 0,alignx left,aligny center");
+		pnImporteInicio.setLayout(new GridLayout(2, 1, 0, 0));
+
+		lblImporte = new JLabel("Importe del pedido a pagar");
+		lblImporte.setFont(new Font("Tahoma", Font.BOLD, 16));
+		pnImporteInicio.add(lblImporte);
+
+		textImporte = new JTextField();
+		textImporte.setBackground(Color.LIGHT_GRAY);
+		textImporte.setEditable(false);
+		textImporte.setFont(new Font("Tahoma", Font.BOLD, 16));
+		pnImporteInicio.add(textImporte);
+		textImporte.setColumns(10);
+
 		btnAtrasInicio = new JButton("Atr\u00E1s");
 		btnAtrasInicio.setFont(new Font("Tahoma", Font.BOLD, 16));
 		btnAtrasInicio.setBackground(Color.RED);
-		pnBotonesInicio.add(btnAtrasInicio);
-		
+		pnBotonesInicio.add(btnAtrasInicio, "cell 1 0,alignx left,aligny center");
+
 		btnSiguienteInicio = new JButton("Siguiente");
 		btnSiguienteInicio.setFont(new Font("Tahoma", Font.BOLD, 16));
 		btnSiguienteInicio.setBackground(new Color(50, 205, 50));
-		pnBotonesInicio.add(btnSiguienteInicio);
-		
+		pnBotonesInicio.add(btnSiguienteInicio, "cell 2 0,alignx left,aligny center");
+
 		frmPago = new JDialog();
 		frmPago.setTitle("Tienda online: pago de pedido");
 		frmPago.setName("Tienda online: pago de pedido");
@@ -121,110 +161,169 @@ public class PagoPedidoView extends JDialog{
 		frmPago.setLocationRelativeTo(null);
 		frmPago.getContentPane().setLayout(new CardLayout(0, 0));
 		frmPago.setModal(true);
-		frmPago.getContentPane().add(pnInicio,"Inicio"); 
-		
+		frmPago.getContentPane().add(pnInicio, "Inicio");
+
 		pnTarjeta = new JPanel();
 		frmPago.getContentPane().add(pnTarjeta, "Tarjeta");
 		pnTarjeta.setLayout(new MigLayout("", "[576px]", "[204px][99px]"));
-		
+
 		pnInfoTarjeta = new JPanel();
+		pnInfoTarjeta.setBorder(new LineBorder(new Color(0, 0, 0), 3));
 		pnTarjeta.add(pnInfoTarjeta, "cell 0 0,grow");
 		pnInfoTarjeta.setLayout(new MigLayout("", "[576px]", "[40px][40px][40px][40px]"));
-		
+
 		lblNumero = new JLabel("N\u00FAmero tarjeta:");
 		lblNumero.setVerticalAlignment(SwingConstants.BOTTOM);
 		lblNumero.setFont(new Font("Tahoma", Font.BOLD, 16));
 		pnInfoTarjeta.add(lblNumero, "cell 0 0,grow");
-		
+
 		textNumeroTarjeta = new JTextField();
 		textNumeroTarjeta.setFont(new Font("Tahoma", Font.BOLD, 16));
 		textNumeroTarjeta.setColumns(10);
 		pnInfoTarjeta.add(textNumeroTarjeta, "cell 0 1,grow");
-		
+
 		lblCVV = new JLabel("CVV:");
 		lblCVV.setVerticalAlignment(SwingConstants.BOTTOM);
 		lblCVV.setFont(new Font("Tahoma", Font.BOLD, 16));
 		pnInfoTarjeta.add(lblCVV, "cell 0 2,grow");
-		
+
 		textCVV = new JTextField();
 		textCVV.setFont(new Font("Tahoma", Font.BOLD, 16));
 		textCVV.setColumns(10);
 		pnInfoTarjeta.add(textCVV, "cell 0 3,grow");
-		
+
 		pnBotonesTarjeta = new JPanel();
+		pnBotonesTarjeta.setBorder(new LineBorder(new Color(0, 0, 0), 3));
 		pnTarjeta.add(pnBotonesTarjeta, "cell 0 1,grow");
-		pnBotonesTarjeta.setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 35));
-		
+		pnBotonesTarjeta.setLayout(new MigLayout("", "[350px,center][75px][109px]", "[54px,grow]"));
+
+		pnImporteTarjeta = new JPanel();
+		pnImporteTarjeta.setBorder(new LineBorder(new Color(0, 0, 0)));
+		pnBotonesTarjeta.add(pnImporteTarjeta, "cell 0 0,alignx left,aligny top");
+		pnImporteTarjeta.setLayout(new GridLayout(2, 1, 0, 0));
+
+		lblImporteTarjeta = new JLabel("Importe del pedido a pagar");
+		lblImporteTarjeta.setFont(new Font("Tahoma", Font.BOLD, 16));
+		pnImporteTarjeta.add(lblImporteTarjeta);
+
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		textField_1.setEditable(false);
+		textField_1.setColumns(10);
+		textField_1.setBackground(Color.LIGHT_GRAY);
+		pnImporteTarjeta.add(textField_1);
+
 		btnAtrasTarjeta = new JButton("Atr\u00E1s");
 		btnAtrasTarjeta.setFont(new Font("Tahoma", Font.BOLD, 16));
 		btnAtrasTarjeta.setBackground(Color.RED);
-		pnBotonesTarjeta.add(btnAtrasTarjeta);
-		
+		pnBotonesTarjeta.add(btnAtrasTarjeta, "cell 1 0,alignx left,aligny center");
+
 		btnSiguienteTarjeta = new JButton("Siguiente");
 		btnSiguienteTarjeta.setFont(new Font("Tahoma", Font.BOLD, 16));
 		btnSiguienteTarjeta.setBackground(new Color(50, 205, 50));
-		pnBotonesTarjeta.add(btnSiguienteTarjeta);
-		
+		pnBotonesTarjeta.add(btnSiguienteTarjeta, "cell 2 0,alignx left,aligny center");
+
 		pnTransaccion = new JPanel();
 		frmPago.getContentPane().add(pnTransaccion, "Transaccion");
 		pnTransaccion.setLayout(new MigLayout("", "[562px]", "[204px][99px]"));
-		
+
 		pnInfoTransaccion = new JPanel();
+		pnInfoTransaccion.setBorder(new LineBorder(new Color(0, 0, 0), 3));
 		pnTransaccion.add(pnInfoTransaccion, "cell 0 0,alignx center,aligny center");
 		pnInfoTransaccion.setLayout(new MigLayout("", "[562px]", "[][]"));
 		pnInfoTransaccion.add(getLblInformacinParaRealizar(), "cell 0 0,grow");
 		pnInfoTransaccion.add(getScpTransaccion(), "cell 0 1,grow");
-		
+
 		pnBotonesTransaccion = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) pnBotonesTransaccion.getLayout();
-		flowLayout.setAlignment(FlowLayout.RIGHT);
-		flowLayout.setVgap(35);
-		flowLayout.setHgap(10);
+		pnBotonesTransaccion.setBorder(new LineBorder(new Color(0, 0, 0), 3));
 		pnTransaccion.add(pnBotonesTransaccion, "cell 0 1,grow");
-		
+		pnBotonesTransaccion.setLayout(new MigLayout("", "[350px,center][75px][109px]", "[54px,grow]"));
+
+		pnImporteTransaccion = new JPanel();
+		pnImporteTransaccion.setBorder(new LineBorder(new Color(0, 0, 0)));
+		pnBotonesTransaccion.add(pnImporteTransaccion, "cell 0 0,alignx left,aligny top");
+		pnImporteTransaccion.setLayout(new GridLayout(2, 1, 0, 0));
+
+		lblImporteTransaccion = new JLabel("Importe del pedido a pagar");
+		lblImporteTransaccion.setFont(new Font("Tahoma", Font.BOLD, 16));
+		pnImporteTransaccion.add(lblImporteTransaccion);
+
+		textImporteTransaccion = new JTextField();
+		textImporteTransaccion.setFont(new Font("Tahoma", Font.BOLD, 16));
+		textImporteTransaccion.setEditable(false);
+		textImporteTransaccion.setColumns(10);
+		textImporteTransaccion.setBackground(Color.LIGHT_GRAY);
+		pnImporteTransaccion.add(textImporteTransaccion);
+
 		btnAtrasTransaccion = new JButton("Atr\u00E1s");
 		btnAtrasTransaccion.setFont(new Font("Tahoma", Font.BOLD, 16));
 		btnAtrasTransaccion.setBackground(Color.RED);
-		pnBotonesTransaccion.add(btnAtrasTransaccion);
-		
+		pnBotonesTransaccion.add(btnAtrasTransaccion, "cell 1 0,alignx left,aligny center");
+
 		btnSiguienteTransaccion = new JButton("Siguiente");
 		btnSiguienteTransaccion.setFont(new Font("Tahoma", Font.BOLD, 16));
 		btnSiguienteTransaccion.setBackground(new Color(50, 205, 50));
-		pnBotonesTransaccion.add(btnSiguienteTransaccion);
-	
+		pnBotonesTransaccion.add(btnSiguienteTransaccion, "cell 2 0,alignx left,aligny center");
+
 	}
-	
-	
-	public JPanel getPnMetodosPago() {return pnMetodosPago;}
-	public JRadioButton getRdbTransferencia() {return this.rdbTransferencia;}
-	public JRadioButton getRdbContrareembolso() {return this.rdbContrareembolso;}
-	public JRadioButton getRdbTarjeta() { return this.rdbTarjeta;}
-	public JDialog getFrame() { return this.frmPago;}
-	public JButton getBtnAtrasInicio() { return this.btnAtrasInicio;}
-	public JButton getBtnSiguienteInicio() { return this.btnSiguienteInicio;}
-	public JButton getBtnAtrasTarjeta() { return this.btnAtrasTarjeta;}
-	public JButton getBtnSiguienteTarjeta() { return this.btnSiguienteTarjeta;}
-	public JButton getBtnAtrasTransaccion() { return this.btnAtrasTransaccion;}
-	public JButton getBtnSiguienteTransaccion() { return this.btnSiguienteTransaccion;}
-	public JTextField getTextNumeroTarjeta() { return this.textNumeroTarjeta;}
-	public JTextField getTextCVV() { return this.textCVV;}
-	private JScrollPane getScpTransaccion() {
-		if (scpTransaccion == null) {
-			scpTransaccion = new JScrollPane();
-			
-			textAreaTransaccion = new JTextArea();
-			textAreaTransaccion.setEditable(false);
-			textAreaTransaccion.setText("Debe realizar un ingreso con el correspondiente importe del pedido al siguiente n\u00FAmero de cuenta: \r\n\r\nXXXX-XXXX-XX-XXXXXXXXXX ");
-			scpTransaccion.setViewportView(textAreaTransaccion);
-		}
+
+	public JPanel getPnMetodosPago() {
+		return pnMetodosPago;
+	}
+
+	public JRadioButton getRdbTransferencia() {
+		return this.rdbTransferencia;
+	}
+
+	public JRadioButton getRdbContrareembolso() {
+		return this.rdbContrareembolso;
+	}
+
+	public JRadioButton getRdbTarjeta() {
+		return this.rdbTarjeta;
+	}
+
+	public JDialog getFrame() {
+		return this.frmPago;
+	}
+
+	public JButton getBtnAtrasInicio() {
+		return this.btnAtrasInicio;
+	}
+
+	public JButton getBtnSiguienteInicio() {
+		return this.btnSiguienteInicio;
+	}
+
+	public JButton getBtnAtrasTarjeta() {
+		return this.btnAtrasTarjeta;
+	}
+
+	public JButton getBtnSiguienteTarjeta() {
+		return this.btnSiguienteTarjeta;
+	}
+
+	public JButton getBtnAtrasTransaccion() {
+		return this.btnAtrasTransaccion;
+	}
+
+	public JButton getBtnSiguienteTransaccion() {
+		return this.btnSiguienteTransaccion;
+	}
+
+	public JTextField getTextNumeroTarjeta() {
+		return this.textNumeroTarjeta;
+	}
+
+	public JTextField getTextCVV() {
+		return this.textCVV;
+	}
+
+	public JScrollPane getScpTransaccion() {
 		return scpTransaccion;
 	}
-	private JLabel getLblInformacinParaRealizar() {
-		if (lblInformacinParaRealizar == null) {
-			lblInformacinParaRealizar = new JLabel("Informaci\u00F3n para realizar la transacci\u00F3n:");
-			lblInformacinParaRealizar.setVerticalAlignment(SwingConstants.BOTTOM);
-			lblInformacinParaRealizar.setFont(new Font("Tahoma", Font.BOLD, 16));
-		}
+
+	public JLabel getLblInformacinParaRealizar() {
 		return lblInformacinParaRealizar;
 	}
 }
