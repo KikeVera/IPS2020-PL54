9d8149e6e9ce3312d361903ac6721cfcd9ac072d
Moises <uo270824@uniovi.es>
2020-11-25 10:02:36 +0100

    Modificación estructura jerarquia productos.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -101,8 +101,7 @@ insert into Pertenece(idProducto,nombreSubcategoria) values
 	(103,'Cableado'),
 	(104,'Bolígrafos'),
 	(105,'Audio'),
-	(106,'Pruebas'),
-	(107,'Pruebas');
+	(100,'Periféricos');
 insert into Producto(id,nombre,descripcion,precioNormal,precioEmpresa,pasillo,estanteria,altura,nombreCategoria,stock,stockMin,stockReposicion) values 
 	(100,'Impresora HP Laserjet','Imprime cualquier documento a la mejor calidad.',122.22,110.12,0,1,3,'Oficina',4,1,10),
 	(101,'Pack de 6 bolígrafos BIC tinta azul','Bolígrafos bic de tinta azul a estrenar.',3.2,2.75,1,1,0,'Oficina',4,1,10),
@@ -110,8 +109,8 @@ insert into Producto(id,nombre,descripcion,precioNormal,precioEmpresa,pasillo,es
 	(103,'Cable USB','3 metros y de color negro.',10.30,5.25,5,4,2,'Informatica',4,1,10),
 	(104,'Bolígrafo borrable rojo','Con tinta roja que se puede borrar.',1.95,1,1,2,2,'Oficina',4,1,10),
 	(105,'Auriculares Corsair','Con excelente calida de sonido.',95.22,90.99,1,4,1,'Informatica',4,1,10),
-    (106,'Producto G','G',14.34,14,1,2,5,'Pruebas',4,1,10),
-    (107,'Producto H','H',8.5,8.25,1,2,7,'Pruebas',4,1,10),
+    	(106,'Producto G','G',14.34,14,1,2,5,'Pruebas',4,1,10),
+    	(107,'Producto H','H',8.5,8.25,1,2,7,'Pruebas',4,1,10),
 	(108,'Pisapapeles','Circular',2,1.25,2,2,7,'Oficina',4,1,10);
 insert into Usuario(idUsuario,tipo,direccion) values 
 	('usuario','Particular','calle1'),
